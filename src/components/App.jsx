0f6b13207033ffcc8bc8ee8c3c1129eaca89166d
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-10-17 17:58:15 +0300

    Add full version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,69 @@
-import React, { Component } from "react";
-import { Statistics } from "./Statistics/Statistics";
-import {FeedbackOptions} from './FeedbackOptions/FeedbackOptions';
-import {Section} from '../components/Section/Section';
-import { Notification } from "./Notification/Notification";
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from '../components/Section/Section';
+import { Notification } from './Notification/Notification';
 // ===========================
 
-export class App extends Component{
-  state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-  }
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleButton = (e) => {
+  const options = {
+    good,
+    neutral,
+    bad,
+  };
+
+  const handleButton = e => {
+    console.log(e.target.name);
     const { name } = e.target;
-    this.setState(prevState=>({
-      [name]: prevState[name]+1
-    }))
-  }
+    switch (name) {
+      case "good":
+        setGood(pGood => pGood + 1);
+        break;
+      case "neutral":
+        setNeutral(pNeutral => pNeutral + 1);
+        break;
+      case "bad":
+        setBad(pBad => pBad + 1);
+        break;
 
-  countTotalFeedback = () => { 
-    const { good, neutral, bad } = this.state; 
-    return (neutral + good + bad);
-  }
-  
-  countPositiveFeedbackPercentage = () => {
-    const percent = Math.round((this.state.good * 100) / this.countTotalFeedback());
+      default:
+        return null;  
+    }    
+  };
+
+  const countTotalFeedback = () => {
+    return neutral + good + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const percent = Math.round((good * 100) / countTotalFeedback());
     if (Number.isNaN(percent)) return 0;
     return percent;
   };
-  
-  render() {
-    const { good, neutral, bad } = this.state;   
-    
-    return (
-      <>
-      <Section title="Please leave feedback" >
-        <FeedbackOptions options={this.state} handleButton={this.handleButton}/>
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} handleButton={handleButton} />
       </Section>
 
       <Section title="Statistics">
-        {this.countTotalFeedback()
-        ?<Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback} positivePercentage={this.countPositiveFeedbackPercentage}/>
-        :<Notification message="There is no feedback"/>
-        }               
-      </Section>      
-      </>
-    );  
-      
-  }
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
+  );
 };
-
-
-
-
-
-
-
-
-/* <h2>Please leave feedback</h2>
-        <button type="button" name="good" onClick={this.handleButton}>Good</button>
-        <button type="button" name="neutral" onClick={this.handleButton}>Neutral</button>
-        <button type="button" name="bad" onClick={this.handleButton}>Bad</button> */
-/*
- handleGoodIncrement = () => {
-    this.setState({
-      good: this.state.good + 1
-    });
-  }
-
-  handleNeutralIncrement = () => {
-    this.setState({
-      neutral: this.state.neutral + 1
-    });
-  }
-
-  handleBadIncrement = () => {
-    this.setState({
-      bad: this.state.bad + 1
-    });
-  }*/
-
-
-  /* <h3>Statistics</h3>
-        <p>Good:<span>{good}</span></p>
-        <p>Neutral:<span>{neutral}</span></p>
-        <p>Bad:<span>{bad}</span></p>
-        <p>Total:<span>{this.countTotalFeedback()}</span></p>
-        <p>Positive feedback:<span>{this.countPositiveFeedbackPercentage()}%</span></p> */
